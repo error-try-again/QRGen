2ce44fe7eb8be159727deab3307cdab0ab96e76e
void <yane.clarke@gmail.com>
2023-10-21 03:57:31 +1000

    Client: - Patched donate link

--- src/components/links/donate.tsx
+++ src/components/links/donate.tsx
@@ -7,7 +7,9 @@ export const Donate: React.FC = () => {
         <p style={donateText}>If you like this tool, please consider donating to help support all future
             development. 📈</p>
         <button style={generateButton}
-                onClick={() => window.open('https://www.paypal.com/donate/?business=FJ6ZMVQEMSU7S&no_recurring=0&item_name=Help+support+future+development%21&currency_code=AUD', '_blank')}>Donate
+                onClick={() => window.open(
+                    'https://www.paypal.com/donate/?business=FJ6ZMVQEMSU7S&no_recurring=1&item_name=Help+support+future+development%21&currency_code=AUD',
+                    '_blank')}>Donate
         </button>
     </div>;
 };
