42ef574719acbc0c2c2469270e9329638af857ef
void <yane.clarke@gmail.com>
2023-11-16 02:04:20 +1000

    Installer/Server
    - Updated certbot renewal mechanism

--- generate_certbot_renewal.sh
+++ generate_certbot_renewal.sh
@@ -1,76 +1,80 @@
 #!/bin/bash
 
-# Ensure the .env file exists
-if [ ! -f .env ]; then
-  echo "The .env file does not exist, exiting..."
-  exit 1
-fi
+#######################################
+# description
+# Globals:
+#   PROJECT_LOGS_DIR
+#   PROJECT_ROOT_DIR
+#   cron_job
+#   cron_log_path
+#   cron_script_path
+# Arguments:
+#  None
+#######################################
+generate_certbot_renewal_job() {
 
-# Source the .env file to define project environment variables
-. .env
-
-# Ensure the project root directory exists
-mkdir -p "${PROJECT_ROOT_DIR}"
-
-# Ensure the log directory exists
-mkdir -p "${PROJECT_LOGS_DIR}"
-
-# Create the certbot renew script with a heredoc
-cat << 'EOF' > "${PROJECT_ROOT_DIR}/certbot_renew.sh"
+  # Create the certbot renew script with a heredoc
+  cat << 'EOF' > "${PROJECT_ROOT_DIR}/certbot_renew.sh"
 #!/bin/bash
+
+# Exit on any error
+set -e
 
 # Load the environment variables
 . "${PROJECT_ROOT_DIR}/.env"
 
-# Check if the docker environment script exists before sourcing
-if [ -f "${PROJECT_ROOT_DIR}/shell-scripts/docker/test-docker-env.sh" ]; then
-  . "${PROJECT_ROOT_DIR}/shell-scripts/docker/test-docker-env.sh"
-else
-  echo "Docker environment script not found, exiting..."
-  exit 1
-fi
-
-LOG_FILE="${PROJECT_LOGS_DIR}/certbot_\$(date +'%Y%m%d_%H%M%S').log"
-
 # Ensure the log directory exists
 mkdir -p "${PROJECT_LOGS_DIR}"
 
-# Enter the project root directory, exit if it fails
-cd "${PROJECT_ROOT_DIR}" || exit 1
+LOG_FILE="${PROJECT_LOGS_DIR}/certbot_\$(date +'%Y%m%d_%H%M%S').log"
 
-# Test the Docker environment, exit if it fails
-test_docker_env || exit 1
+# Function to restart services with Docker Compose
+restart_services() {
+  local service_name="$1"
+  if docker compose -f "$compose_file" up -d "$service_name"; then
+    echo "Service $service_name restarted successfully."
+  else
+    echo "ERROR: Failed to restart the service $service_name."
+    exit 1
+  fi
+}
+
+# Function to perform a certbot renewal
+renew_certbot() {
+  # Run the certbot service with dry run first
+  docker compose run --rm certbot renew --dry-run
+
+  # If the dry run succeeds, run certbot renewal without dry run
+  echo "Certbot dry run succeeded, attempting renewal..."
+  docker compose run --rm certbot renew
+
+  # Restart the nginx frontend and backend services
+  restart_services "frontend"
+  restart_services "backend"
+}
 
 # Start logging
 {
   echo "Running certbot renewal script on \$(date)"
 
-  # Run the certbot service with dry run first and check if it succeeds
-  if docker compose run --rm certbot renew --dry-run; then
-    # If the dry run succeeds, run certbot renewal without dry run
-    echo "Certbot dry run succeeded, attempting renewal..."
-    docker compose run --rm certbot renew
-  else
-    echo "Certbot dry run failed, skipping renewal."
-    exit 1
-  fi
+  renew_certbot
 } | tee -a "${LOG_FILE}" # Append output to log file
 EOF
 
-# Make the certbot renew script executable
-chmod +x "${PROJECT_ROOT_DIR}/certbot_renew.sh"
+  # Make the certbot renew script executable
+  chmod +x "${PROJECT_ROOT_DIR}/certbot_renew.sh"
 
-# The path to the cron job script
-cron_script_path="${PROJECT_ROOT_DIR}/certbot_renew.sh"
-cron_log_path="${PROJECT_LOGS_DIR}/certbot_cron.log"
+  # Setup Cron Job
+  local cron_script_path="${PROJECT_ROOT_DIR}/certbot_renew.sh"
+  local cron_log_path="${PROJECT_LOGS_DIR}/certbot_cron.log"
 
-# The crontab entry to be added
-cron_job="0 0 * * * ${cron_script_path} >> ${cron_log_path} 2>&1"
+  # Cron job to run certbot renewal every weekday at midnight (certbot limits to 5 renewals per week)
+  local cron_job="0 0 * * 1-5 ${cron_script_path} >> ${cron_log_path} 2>&1"
 
-# Check and add the cron job if it doesn't exist
-if ! crontab -l | grep -Fxq "$cron_job"; then
+  # Add the cron job if it doesn't exist
   (
     crontab -l 2> /dev/null
-    echo "$cron_job"
+                          echo "$cron_job"
   ) | crontab -
-fi
+
+}
