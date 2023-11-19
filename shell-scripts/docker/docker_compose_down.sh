1aa1a6e4619c37ef74986c9f3505640ae1e03407
void <yane.clarke@gmail.com>
2023-11-19 20:58:26 +1000

    Installer:
    - Modified stop containers check

--- shell-scripts/docker/docker_compose_down.sh
+++ shell-scripts/docker/docker_compose_down.sh
@@ -13,14 +13,14 @@ stop_containers() {
   local containers_to_stop
   containers_to_stop=$(docker ps -a -q --filter "name=qrgen")
 
-  if docker_compose_exists && [ -f "$docker_compose_file" ]; then
+  if docker_compose_exists; then
     echo "Stopping containers using docker-compose..."
-    docker compose -f "$docker_compose_file" down
+    docker compose -f "${PROJECT_ROOT_DIR}/docker-compose.yml" down
   fi
 
   if [ -n "$containers_to_stop" ]; then
     echo "Force stopping remaining 'qrgen' containers..."
-    docker stop $containers_to_stop
+    docker stop "${containers_to_stop}"
   else
     echo "No 'qrgen' containers to stop."
   fi
