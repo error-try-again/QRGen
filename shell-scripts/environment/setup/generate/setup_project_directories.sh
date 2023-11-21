368bdf62de8f3ec339ef08e8eaf3a48d09574605
void <yane.clarke@gmail.com>
2023-11-21 11:43:53 +1000

    Installer:
    - Patched a bug prevented sources from being copied on local installations

--- shell-scripts/environment/setup/generate/setup_project_directories.sh
+++ shell-scripts/environment/setup/generate/setup_project_directories.sh
@@ -1,32 +1,11 @@
 #!/bin/bash
-
-#######################################
-# description
-# Globals:
-#   BACKEND_DIR
-#   CERTBOT_DIR
-#   FRONTEND_DIR
-#   HOME
-#   PROJECT_LOGS_DIR
-#   SERVER_DIR
-# Arguments:
-#  None
-#######################################
 setup_project_directories() {
   echo "Staging project directories..."
-
   local directory
+
   for directory in "$SERVER_DIR" "$FRONTEND_DIR" "$BACKEND_DIR" "$CERTBOT_DIR" "$PROJECT_LOGS_DIR"; do
     create_directory "$directory"
   done
 
-  local src_dir="$HOME/QRGen/src"
-  local server_src_dir="$HOME/QRGen/server"
-
-  if [[ -d $src_dir && -d $server_src_dir ]]; then
-    copy_server_files
-  else
-    echo "Error: Sources are not available, exiting..."
-    exit 1
-  fi
+  copy_server_files
 }
