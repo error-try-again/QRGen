58a82af51c50b04e19fbe3dbeb1ba33c42c1ea7c
void <yane.clarke@gmail.com>
2023-12-29 14:51:17 +1000

    - Refactored & Modularized compose, docker & nginx config mocking & testing
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

---
+++ scripts/tests/logging/log_mock_error.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# bashsupport disable=BP5006
+
+set -euo pipefail
+
+#######################################
+# Log an error message to a file if it contains a common error signature
+# Globals:
+#   test_output_dir
+# Arguments:
+#   1
+#######################################
+function log_mock_error() {
+  local message=$1
+  local error_log="${test_output_dir}/error.log"
+
+  # Define common error signatures to catch
+  local common_errors=("Error" "Failed" "Cannot" "Denied" "Unbound" "Timeout" "Refused" "Invalid" "Bad" "Unknown" "Not Found" "Not Available" "Not Allowed")
+
+  local error
+  for error in "${common_errors[@]}"; do
+    if [[ ${message} == *${error}* ]]; then
+      local date
+      date=$(date +"%Y-%m-%d %H:%M:%S")
+      echo "${date}: ${message}" >> "${error_log}"
+    fi
+  done
+}
