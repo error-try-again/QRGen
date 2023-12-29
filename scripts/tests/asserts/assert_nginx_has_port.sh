94022755d0ee7f73b1328e2e4bbc98531238041c
void <yane.clarke@gmail.com>
2023-12-29 14:52:08 +1000

    - Refactored & Modularized compose, docker & nginx config mocking & testing
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

---
+++ scripts/tests/asserts/assert_nginx_has_port.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# bashsupport disable=BP5006
+
+set -euo pipefail
+
+#######################################
+# Assert that NGINX config has defined port(s)
+# Arguments:
+#   $1 - NGINX configuration file
+#######################################
+function assert_nginx_has_port() {
+  local file=$1
+
+    # Implement a check for port definition in NGINX config
+  if ! grep -qE 'listen[[:space:]]+[0-9]+' "${file}"; then
+        log_mock_error "missing_port"
+  fi
+}
