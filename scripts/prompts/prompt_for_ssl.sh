a66d6e945110c95a97d857e86d67117bcdb07aea
void <yane.clarke@gmail.com>
2023-12-29 13:07:29 +1000

    - Improved letsencrypt procedural prompts
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

--- scripts/prompts/prompt_for_ssl.sh
+++ scripts/prompts/prompt_for_ssl.sh
@@ -10,17 +10,13 @@ set -euo pipefail
 # Arguments:
 #  None
 #######################################
-prompt_for_ssl() {
-  echo "1: Use Let's Encrypt SSL"
-  echo "2: Use self-signed SSL certificates"
-  echo "3: Do not enable SSL"
-  prompt_numeric "Please enter your choice (1/2/3): " SSL_CHOICE
-  case $SSL_CHOICE in
-    1) prompt_for_letsencrypt_options ;;
-    2) enable_ssl ;;
-    3) echo "SSL will not be enabled." ;;
-    *)
-       echo "Invalid choice, please enter 1, 2, or 3."
-                                                       ;;
+function prompt_for_ssl() {
+  echo "1: Enable SSL"
+  echo "2: Do not enable SSL"
+  prompt_numeric "Please enter your choice (1/2): " SSL_CHOICE
+  case ${SSL_CHOICE} in
+    1) enable_ssl ;;
+    2) echo "SSL will not be enabled." ;;
+    *) echo "Invalid choice, please enter 1 or 2." ;;
   esac
 }
