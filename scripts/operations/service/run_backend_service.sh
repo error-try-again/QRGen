436f13129996517fbc867a6f1c7191e5d41d9ac4
void <yane.clarke@gmail.com>
2023-12-29 21:20:03 +1000

    - Refactored for modularity
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

--- scripts/operations/service/run_backend_service.sh
+++ scripts/operations/service/run_backend_service.sh
@@ -2,9 +2,24 @@
 
 set -euo pipefail
 
+
 #######################################
-# Builds and runs the backend service
-# Checks to see if caching is disabled, and builds accordingly
+# Builds and runs the backend service using docker compose.
+# Arguments:
+#   1
+#######################################
+function docker_compose_backend_service() {
+  local cache_option=$1
+  echo "Building and running Backend service ${cache_option}..."
+  if ! docker compose --progress=plain build "${cache_option}" backend; then
+    echo "Failed to build Backend service."
+    exit 1
+  fi
+  docker compose up -d backend
+}
+
+#######################################
+# Determines whether Docker's caching system should be used when building the Backend service image.
 # Globals:
 #   PROJECT_ROOT_DIR
 # Arguments:
@@ -12,18 +27,8 @@ set -euo pipefail
 #######################################
 function run_backend_service() {
   if [[ -n ${DISABLE_DOCKER_CACHING} ]]; then
-    echo "Building and running Backend service without caching..."
-    if ! docker compose --progress=plain build --no-cache backend; then
-      echo "Failed to build Backend service."
-      exit 1
-    fi
-    docker compose up -d backend
+    docker_compose_backend_service '--no-cache'
   else
-    echo "Building and running Backend service..."
-    if ! docker compose --progress=plain build backend; then
-      echo "Failed to build Backend service."
-      exit 1
-    fi
-    docker compose up -d backend
+    docker_compose_backend_service ''
   fi
 }
