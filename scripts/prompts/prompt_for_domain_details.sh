33baa007d3dc81c8fe61b88e12c4868e41adfef1
void <yane.clarke@gmail.com>
2023-12-29 15:58:41 +1000

    - Refactor to prefer curly braces    - Added guard clause for installer profile resolution
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

--- scripts/prompts/prompt_for_domain_details.sh
+++ scripts/prompts/prompt_for_domain_details.sh
@@ -17,7 +17,7 @@ set -euo pipefail
 #  None
 #######################################
 function prompt_for_domain_details() {
-  if [[ ${SKIP_DOMAIN_PROMPT} == true ]]; then
+  if [[ ${SKIP_DOMAIN_PROMPT} == "true" ]]; then
      echo "Skipping domain prompt, please ensure that the following environment variables are set correctly:"
       echo "  BACKEND_SCHEME: ${BACKEND_SCHEME}"
       echo "  DOMAIN_NAME: ${DOMAIN_NAME}"
@@ -28,22 +28,22 @@ function prompt_for_domain_details() {
     return
   fi
   prompt_yes_no "Would you like to specify a domain name other than the default (http://localhost)" USE_CUSTOM_DOMAIN
-  if [[ ${USE_CUSTOM_DOMAIN} == true ]]; then
+  if [[ ${USE_CUSTOM_DOMAIN} == "true" ]]; then
     DOMAIN_NAME=$(prompt_with_validation "Enter your domain name (e.g., example.com): " "Error: Domain name cannot be empty.")
     local origin_url="${BACKEND_SCHEME}://${DOMAIN_NAME}"
     ORIGIN="${origin_url}:${ORIGIN_PORT}"
     echo "Using custom domain name: ${origin_url}"
 
     prompt_yes_no "Would you like to specify a subdomain other than the default (none)" USE_SUBDOMAIN
-    if [[ ${USE_SUBDOMAIN} == true ]]; then
+    if [[ ${USE_SUBDOMAIN} == "true" ]]; then
       SUBDOMAIN=$(prompt_with_validation "Enter your subdomain name (e.g., www): " "Error: Subdomain name cannot be empty.")
       origin_url="${BACKEND_SCHEME}://${SUBDOMAIN}.${DOMAIN_NAME}"
       ORIGIN="${origin_url}:${ORIGIN_PORT}"
       echo "Using custom subdomain: ${origin_url}"
-    fi
-  else
+  fi
+else
     DOMAIN_NAME="localhost"
     ORIGIN="${BACKEND_SCHEME}://${DOMAIN_NAME}:${ORIGIN_PORT}"
     echo "Using default domain name: ${ORIGIN}"
-  fi
+fi
 }
