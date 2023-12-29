3db0d63f18c6cc146d85500cc0b1e009c74a8a70
void <yane.clarke@gmail.com>
2023-12-29 20:47:48 +1000

    - Added guard clause to handle dhparam regen from installer profile    - Refactored and modularized generating self signed certificates logic
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

--- scripts/operations/certificates/generate_self_signed_certificates.sh
+++ scripts/operations/certificates/generate_self_signed_certificates.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -eo pipefail
 
 #######################################
 # Handles openssl certificate generation, and dh parameters generation.
@@ -16,34 +16,59 @@ set -euo pipefail
 function generate_self_signed_certificates() {
   echo "Generating self-signed certificates for ${DOMAIN_NAME}..."
 
-  local certs_path=${CERTS_DIR}/live/${DOMAIN_NAME}
+  export certs_path=${CERTS_DIR}/live/${DOMAIN_NAME}
+  local fullchain_path="${certs_path}/fullchain.pem"
+  local privkey_path="${certs_path}/privkey.pem"
+
+  local dh_params_path="${CERTS_DH_DIR}/dhparam.pem"
 
   # Ensure the necessary directories exist
   create_directory "${certs_path}"
   create_directory "${CERTS_DH_DIR}"
 
-  local dh_params_path="${CERTS_DH_DIR}/dhparam.pem"
+  check_and_generate_certificates "${fullchain_path}" "${privkey_path}"
+  generate_dh_parameters "${dh_params_path}"
+}
 
-  if [[ ! -f "${dh_params_path}" ]]; then
-    echo "DH parameters file not found at ${dh_params_path}."
-fi
+#######################################
+# description
+# Globals:
+#   DOMAIN_NAME
+#   certs_path
+# Arguments:
+#   1K
+#   2
+#######################################
+function check_and_generate_certificates() {
+  local fullchain_path=$1
+  local privkey_path=$2
 
-  # Check and generate new self-signed certificates if needed
-  if [[ ! -f "${certs_path}/fullchain.pem" ]] || prompt_for_dhparam_regeneration "${certs_path}"; then
-    # Create self-signed certificate and private key
+  if [[ ! -f "${fullchain_path}" ]] || prompt_for_dhparam_regeneration "${certs_path}"; then
     openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
-      -keyout "${certs_path}/privkey.pem" \
-      -out "${certs_path}/fullchain.pem" \
+      -keyout "${privkey_path}" \
+      -out "${fullchain_path}" \
       -subj "/CN=${DOMAIN_NAME}"
-
     echo "Self-signed certificates for ${DOMAIN_NAME} generated at ${certs_path}."
-
-    # Generate DH parameters
-    prompt_for_dhparam_strength
-    echo "Generate a Diffie-Hellman (DH) key exchange parameters file with ${DH_PARAM_SIZE} bits..."
-    openssl dhparam -out "${dh_params_path}" "${DH_PARAM_SIZE}"
-    echo "DH parameters generated at ${dh_params_path}."
   else
     echo "Certificates for ${DOMAIN_NAME} already exist at ${certs_path}."
+  fi
+}
+
+#######################################
+# description
+# Globals:
+#   DH_PARAM_SIZE
+# Arguments:
+#   1
+#######################################
+function generate_dh_parameters() {
+  local dh_params_path=$1
+
+  if [[ ! -f "${dh_params_path}" ]]; then
+    echo "DH parameters file not found at ${dh_params_path}. Generating new DH parameters."
+
+    prompt_for_dhparam_strength
+    echo "Generate a Diffie-Hellman (DH) key exchange parameters file with ${DH_PARAM_SIZE} bits..."    openssl dhparam -out "${dh_params_path}" "${DH_PARAM_SIZE}"
+    echo "DH parameters generated at ${dh_params_path}."
   fi
 }
--- scripts/prompts/prompt_for_dhparam_regen.sh
+++ scripts/prompts/prompt_for_dhparam_regen.sh
@@ -13,12 +13,14 @@ set -euo pipefail
 #   1 ...
 #######################################
 function prompt_for_dhparam_regeneration() {
-  if [[ $REGENERATE_SSL_CERTS  ]]; then
+  if [[ ${REGENERATE_SSL_CERTS} == "true" ]]; then
     return 0
+  elif [[ ${REGENERATE_SSL_CERTS} == "false" ]]; then
+    return 1
   fi
   local response
   read -rp "Do you want to regenerate the certificates in $1? [y/N]: " response
-  if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  if [[ ${response} =~ ^([yY][eE][sS]|[yY])$ ]]; then
     return 0
   else
     return 1
