e89af9e7889aabaaa626d7e63a4b5829c47005b7
void <yane.clarke@gmail.com>
2023-12-29 13:02:43 +1000

    - Adjusted code style to prefer putting braces around variable references even when not strictly required.    - Prefer explicit -n to check non-empty string (or use =/-ne to check boolean/integer).    - Patched netcat mock server to shutdown correctly after use
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

--- scripts/prompts/prompt_yes_no.sh
+++ scripts/prompts/prompt_yes_no.sh
@@ -13,18 +13,18 @@ function prompt_yes_no() {
   local result_var="$2"
   local choice
   while true; do
-    read -rp "$prompt_message [Y/n]: " choice
+    read -rp "${prompt_message} [Y/n]: " choice
     case "${choice,,}" in  # Convert to lowercase for easier matching
       yes | y)
-        eval "$result_var=true"
+        eval "${result_var}=true"
         break
         ;;
       no | n)
-        eval "$result_var=false"
+        eval "${result_var}=false"
         break
         ;;
       "")  # Default to 'yes' if the user just presses enter
-        eval "$result_var=true"
+        eval "${result_var}=true"
         break
         ;;
       *) echo "Invalid input. Please enter 'yes' or 'no'." ;;
