e11965f0858a9c3c4a7319a6a13d34ac87e3dbce
void <yane.clarke@gmail.com>
2023-12-29 14:50:07 +1000

    - Refactored & Modularized compose, docker & nginx config mocking & testing
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

---
+++ scripts/tests/mocks/dockerfiles/run_dockerfile_frontend_mock.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# bashsupport disable=BP5006
+
+set -euo pipefail
+
+#######################################
+# Mocks Frontend/React Dockerfile configuration
+#######################################
+function run_dockerfile_frontend_mock() {
+  echo "---------------------------------------"
+  echo "Simulating frontend Dockerfile configuration..."
+
+  FRONTEND_DOCKERFILE="${test_output_dir}/Frontend.Dockerfile"
+
+  configure_frontend_docker 2>&1 | tee "${test_output_dir}/frontend_docker_output.log"
+
+  # Check for errors in the output and log them
+  local docker_output
+  docker_output=$(< "${test_output_dir}/frontend_docker_output.log")
+
+  log_mock_error "${docker_output}"
+}
