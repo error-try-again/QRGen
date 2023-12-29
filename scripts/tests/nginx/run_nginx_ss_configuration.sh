190690160ab960810bc168cd8bdae84a840cfc91
void <yane.clarke@gmail.com>
2023-12-29 14:48:45 +1000

    - Refactored & Modularized compose, docker & nginx config mocking & testing
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

---
+++ scripts/tests/nginx/run_nginx_ss_configuration.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# bashsupport disable=BP5006
+
+#######################################
+# Mocks the self signed certificate generation nginx configuration
+# Globals:
+#   NGINX_CONF_FILE
+#   test_output_dir
+# Arguments:
+#  None
+#######################################
+function run_nginx_ss_mock() {
+  echo "---------------------------------------"
+  echo "Simulating NGINX configuration for self-signed certificates..."
+
+  setup_self_signed_mock_parameters
+
+  DOCKER_COMPOSE_FILE="${test_output_dir}/docker-compose-ss.yml"
+  NGINX_CONF_FILE="${test_output_dir}/nginx-ss.conf"
+
+  configure_nginx_config 2>&1 | tee "${test_output_dir}/nginx_ss_output.log"
+
+  # Check for errors in the output and log them
+  local nginx_output
+  nginx_output=$(< "${test_output_dir}/nginx_ss_output.log")
+
+  log_mock_error "${nginx_output}"
+
+  validate_service_config "${NGINX_CONF_FILE}" "nginx"
+}
