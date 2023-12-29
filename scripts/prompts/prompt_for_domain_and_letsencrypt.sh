5aea1e00e5fed863b22fd0e82a01355fcbf05786
void <yane.clarke@gmail.com>
2023-12-29 13:07:20 +1000

    - Improved letsencrypt procedural prompts
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

--- scripts/prompts/prompt_for_domain_and_letsencrypt.sh
+++ scripts/prompts/prompt_for_domain_and_letsencrypt.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 #######################################
-# Prompts the user to select whether they want to use a custom domain and prompts for the relevant information.
+# Prompt the user for domain and SSL certificate details when setting up the server.
 # Globals:
 #   USE_CUSTOM_DOMAIN
 # Arguments:
@@ -11,8 +11,21 @@ set -euo pipefail
 #######################################
 function prompt_for_domain_and_letsencrypt() {
   prompt_for_domain_details
-  if [[ $USE_CUSTOM_DOMAIN ]]; then
+  decide_on_ssl_or_self_signed
+}
+
+#######################################
+# If a custom domain is in use, it prompts for SSL and LetsEncrypt details along with the corresponding Certbot flags.
+# If a custom domain is not in use, it will prompt for self-signed certificates.
+# Globals:
+#   USE_CUSTOM_DOMAIN
+# Arguments:
+#  None
+#######################################
+function decide_on_ssl_or_self_signed() {
+  if [[ -n ${USE_CUSTOM_DOMAIN} ]]; then
     prompt_for_ssl
+    prompt_for_letsencrypt
     construct_certbot_flags
   else
     prompt_for_self_signed_certificates
--- scripts/prompts/prompt_for_letsencrypt.sh
+++ scripts/prompts/prompt_for_letsencrypt.sh
@@ -4,23 +4,43 @@
 set -euo pipefail
 
 #######################################
-# Prompts the user for the Let's Encrypt email and other Let's Encrypt related settings.
+# Prompts to use Let's Encrypt. If yes, prompts for certbot flag settings & TLS selection.
+# If no, returns to proceed with self-signed certificates.
 # Globals:
+#   USE_LETSENCRYPT
 #   LETSENCRYPT_EMAIL
-#   USE_AUTO_RENEW_SSL
+#   USE_PRODUCTION_SSL
 #   USE_DRY_RUN
 #   USE_FORCE_RENEW
+#   USE_AUTO_RENEW_SSL
 #   USE_HSTS
-#   USE_MUST_STAPLE
 #   USE_OCSP_STAPLING
-#   USE_OVERWRITE_SELF_SIGNED_CERTS
-#   USE_PRODUCTION_SSL
-#   USE_STRICT_PERMISSIONS
+#   USE_MUST_STAPLE
 #   USE_UIR
+#   USE_STRICT_PERMISSIONS
+#   USE_OVERWRITE_SELF_SIGNED_CERTS
+# Arguments:
+#  None
+# Returns:
+#   <unknown> ...
+#######################################
+function prompt_for_letsencrypt() {
+  prompt_yes_no "Would you like to use Let's Encrypt?" USE_LETSENCRYPT
+  if [[ "${USE_LETSENCRYPT}" == "false" ]]; then
+    return
+  else
+    prompt_for_letsencrypt_install_type
+    certbot_prompts
+    prompt_tls_selection
+  fi
+}
+
+#######################################
+# Prompts the user for certbot flag settings.
 # Arguments:
 #  None
 #######################################
-function prompt_for_letsencrypt() {
+function certbot_prompts() {
   prompt_and_validate_input "Please enter your Let's Encrypt email or type 'skip' to skip: " "Error: Email address cannot be empty." LETSENCRYPT_EMAIL
   prompt_yes_no "Would you like to use a production SSL certificate?" USE_PRODUCTION_SSL
   prompt_yes_no "Would you like to use a dry run?" USE_DRY_RUN
@@ -32,5 +52,5 @@ function prompt_for_letsencrypt() {
   prompt_yes_no "Would you like to enable UIR (Unique Identifier for Revocation)?" USE_UIR
   prompt_yes_no "Would you like to enable Strict Permissions (Not Recommended)?" USE_STRICT_PERMISSIONS
   prompt_yes_no "Would you like to overwrite existing certificates?" USE_OVERWRITE_SELF_SIGNED_CERTS
-  prompt_tls_selection
+  prompt_yes_no "Would you like to build with Certbot? (Highly Recommended)" BUILD_CERTBOT_IMAGE
 }
