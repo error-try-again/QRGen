9ebe8447d33641fa37f958392f42a05e2c5c891d
void <yane.clarke@gmail.com>
2023-12-29 13:03:18 +1000

    - Adjusted code style to prefer putting braces around variable references even when not strictly required.    - Prefer explicit -n to check non-empty string (or use =/-ne to check boolean/integer).    - Patched netcat mock server to shutdown correctly after use
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

--- scripts/setup/docker/common/test_docker_env.sh
+++ scripts/setup/docker/common/test_docker_env.sh
@@ -18,7 +18,7 @@ function test_docker_env() {
   # Update or set DOCKER_HOST.
   local expected_docker_host
   expected_docker_host="unix:///run/user/$(id -u)/docker.sock"
-  if [ -z "${DOCKER_HOST:-}" ] || [ "${DOCKER_HOST:-}" != "${expected_docker_host}" ]; then
+  if [[ -z "${DOCKER_HOST:-}" ]] || [[ "${DOCKER_HOST:-}" != "${expected_docker_host}" ]]; then
     DOCKER_HOST="${expected_docker_host}"
     export DOCKER_HOST
     echo "Set DOCKER_HOST to ${DOCKER_HOST}"
