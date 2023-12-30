3f009f160b3fbc6c273b7b6dace917cbd151922b
void <yane.clarke@gmail.com>
2023-12-30 10:10:44 +1000

    - Added arg count error checking in profile application stag    - Removed redundant -n checks that were put in when I was overtired
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

--- scripts/config/profiles/apply_profile.sh
+++ scripts/config/profiles/apply_profile.sh
@@ -14,15 +14,23 @@ set -euo pipefail
 #   The value from the JSON file for the specified key within the given profile.
 #######################################
 function get_config_value() {
-  local profile=$1                                      # The profile within the JSON file.
-  local key=$2                                          # The specific key within the profile.
+
+  if [[ $# -lt 2 ]]; then
+    echo "Error: Not enough arguments"
+    echo "Usage: get_config_value [profile] [key]"
+    exit 1
+  fi
+
+  local profile=$1 # The profile within the JSON file.
+  local key=$2     # The specific key within the profile.
+
   jq -r ".${profile}.${key}" "${JSON_INSTALL_PROFILES}" # Use jq to parse and return the value.
 }
 
 #######################################
 # Applies a configuration profile by setting global variables.
 # This function iterates over keys in a given profile within the JSON configuration file,
-# setting each as a global variable to its corresponding value. It handles boolean values explicitly.
+# setting each as a global variable to its corresponding value.
 # Globals:
 #   JSON_INSTALL_PROFILES - Path to the JSON file containing installer profiles.
 # Arguments:
@@ -33,32 +41,32 @@ function get_config_value() {
 #   It's crucial that JSON_INSTALL_PROFILES is correctly set to the path of the JSON configuration file before this function is called.
 #######################################
 function apply_profile() {
+
+  if [[ $# -lt 1 ]]; then
+    echo "Error: Not enough arguments"
+    echo "Usage: apply_profile [profile]"
+    exit 1
+  fi
+
   local profile=$1 # The name of the profile to apply.
   echo "Applying profile: ${profile}"
 
   # Retrieve the keys from the specified profile within the JSON file.
   local keys
   keys=$(jq -r ".${profile} | keys | .[]" "${JSON_INSTALL_PROFILES}")
-  echo "Found keys in ${profile}: ${keys}"
 
   # Iterate over each key in the profile.
   local key
   for key in ${keys}; do
     local value
+
     # Retrieve the value for the current key from the profile.
     value=$(get_config_value "${profile}" "${key}")
-    echo "Setting ${key}=${value}"
-
-    # Handle boolean values explicitly if needed
-    case "${value}" in
-    true | false) ;; # No operation needed if it's already a boolean
-    *) echo "Setting ${key} to non-boolean value: ${value}" ;;
-    esac
 
     # Declare the key-value pair as a global variable.
     # This is crucial for the profile to be picked up by the installer.
-    # shellcheck disable=SC2034
-    declare -g "${key}=${value}"
+    declare -g "$key=$value"
+
     echo "Applied ${key}=${value}"
   done
 }
--- scripts/flags/construct_certbot_flags.sh
+++ scripts/flags/construct_certbot_flags.sh
@@ -31,13 +31,13 @@ set -euo pipefail
 #######################################
 function construct_certbot_flags() {
   EMAIL_FLAG=$([[ ${LETSENCRYPT_EMAIL} == "skip" ]] && echo "--register-unsafely-without-email" || echo "--email ${LETSENCRYPT_EMAIL}")
-  PRODUCTION_CERTS_FLAG=$([[ -n ${USE_PRODUCTION_SSL} && ${USE_PRODUCTION_SSL} == "true" ]] && echo "" || echo "--staging")
-  DRY_RUN_FLAG=$([[ -n ${USE_DRY_RUN} && ${USE_DRY_RUN} == "true" ]] && echo "--dry-run" || echo "")
-  FORCE_RENEW_FLAG=$([[ -n ${USE_FORCE_RENEW} && ${USE_FORCE_RENEW} == "true" ]] && echo "--force-renewal" || echo "")
-  OVERWRITE_SELF_SIGNED_CERTS_FLAG=$([[ -n ${USE_OVERWRITE_SELF_SIGNED_CERTS} && ${USE_OVERWRITE_SELF_SIGNED_CERTS} == "true" ]] && echo "--overwrite-cert-dirs" || echo "")
-  OCSP_STAPLING_FLAG=$([[ -n ${USE_OCSP_STAPLING} && ${USE_OCSP_STAPLING} == "true" ]] && echo "--staple-ocsp" || echo "")
-  MUST_STAPLE_FLAG=$([[ -n ${USE_MUST_STAPLE} && ${USE_MUST_STAPLE} == "true" ]] && echo "--must-staple" || echo "")
-  STRICT_PERMISSIONS_FLAG=$([[ -n ${USE_STRICT_PERMISSIONS} && ${USE_STRICT_PERMISSIONS} == "true" ]] && echo "--strict-permissions" || echo "")
-  HSTS_FLAG=$([[ -n ${USE_HSTS} && ${USE_HSTS} == "true" ]] && echo "--hsts" || echo "")
-  UIR_FLAG=$([[ -n ${USE_UIR} && ${USE_UIR} == "true" ]] && echo "--uir" || echo "")
+  PRODUCTION_CERTS_FLAG=$([[ ${USE_PRODUCTION_SSL} == "true" ]] && echo "" || echo "--staging")
+  DRY_RUN_FLAG=$([[ ${USE_DRY_RUN} == "true" ]] && echo "--dry-run" || echo "")
+  FORCE_RENEW_FLAG=$([[ ${USE_FORCE_RENEW} == "true" ]] && echo "--force-renewal" || echo "")
+  OVERWRITE_SELF_SIGNED_CERTS_FLAG=$([[ ${USE_OVERWRITE_SELF_SIGNED_CERTS} == "true" ]] && echo "--overwrite-cert-dirs" || echo "")
+  OCSP_STAPLING_FLAG=$([[ ${USE_OCSP_STAPLING} == "true" ]] && echo "--staple-ocsp" || echo "")
+  MUST_STAPLE_FLAG=$([[ ${USE_MUST_STAPLE} == "true" ]] && echo "--must-staple" || echo "")
+  STRICT_PERMISSIONS_FLAG=$([[ ${USE_STRICT_PERMISSIONS} == "true" ]] && echo "--strict-permissions" || echo "")
+  HSTS_FLAG=$([[ ${USE_HSTS} == "true" ]] && echo "--hsts" || echo "")
+  UIR_FLAG=$([[ ${USE_UIR} == "true" ]] && echo "--uir" || echo "")
 }
