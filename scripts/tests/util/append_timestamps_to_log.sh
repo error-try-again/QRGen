477abd037e89cf7a4f39f23648c40aab7e6f550f
void <yane.clarke@gmail.com>
2023-12-30 22:25:51 +1000

    - Addition timestamp amendment support for log files
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

---
+++ scripts/tests/util/append_timestamps_to_log.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#######################################
+# Generates a log file with timestamps for each line.
+# Arguments:
+#   log_file: The log file to add timestamps to
+#######################################
+function append_timestamps_to_log() {
+  local log_file="${1}"
+  if [[ ! -f "${log_file}" ]]; then
+    echo "Error: ${log_file} does not exist!"
+    return 1
+  fi
+  awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' "${log_file}" > "${log_file}.tmp" && \
+  mv "${log_file}.tmp" "${log_file}" || \
+  echo "Error: Failed to process file!"
+}
