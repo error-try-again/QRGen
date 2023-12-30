db2f0729ae51ff8b7614be08e7018ad643c525cd
void <yane.clarke@gmail.com>
2023-12-30 22:29:45 +1000

    - General cleanup & renaming
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

--- scripts/operations/util/generate_file_path.sh
+++ scripts/operations/util/generate_file_path.sh
@@ -2,15 +2,23 @@
 
 set -euo pipefail
 
+#######################################
+# description
+# Arguments:
+#   1
+# Returns:
+#   1 ...
+#######################################
 function generate_file_paths() {
+  if [[ $# -eq 0 ]]; then
+    echo "No arguments supplied"
+    return 1
+  fi
+
   local file_name="${1}"
-
-  if [[ ! -f "${file_name}" ]]; then
-    echo "Nginx configuration file not found: ${file_name}"
-    echo "Generating nginx configuration file..."
-
+  if [[ ! -f ${file_name}   ]]; then
+    echo "File not found: ${file_name} | generating..."
     mkdir -p "$(dirname "${file_name}")"
     touch "${file_name}"
   fi
-
 }
