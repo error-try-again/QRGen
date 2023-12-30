cf9c476547ab9fa2d908d8e9f227c374e7cd731e
void <yane.clarke@gmail.com>
2023-12-30 22:34:07 +1000

    - General cleanup and refactoring
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

--- scripts/tests/mocks/dockerfiles/run_dockerfile_frontend_mock.sh
+++ scripts/tests/mocks/dockerfiles/run_dockerfile_frontend_mock.sh
@@ -9,16 +9,11 @@ set -euo pipefail
 function run_dockerfile_frontend_mock() {
   echo "---------------------------------------"
   echo "Simulating frontend Dockerfile configuration..."
-
   FRONTEND_DOCKERFILE="${test_output_dir}/Frontend.Dockerfile"
-
   generate_file_paths "${FRONTEND_DOCKERFILE}"
-
-  configure_frontend_docker 2>&1 | tee "${test_output_dir}/frontend_docker_output.log"
-
+generate_frontend_dockerfile 2>&1 | tee "${test_output_dir}/frontend_docker_output.log"
   # Check for errors in the output and log them
   local docker_output
   docker_output=$(<"${test_output_dir}/frontend_docker_output.log")
-
   log_mock_error "${docker_output}"
 }
