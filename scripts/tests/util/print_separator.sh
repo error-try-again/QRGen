b18c574b4960e9b13eeda5f81e0ade834f80b212
void <yane.clarke@gmail.com>
2023-12-30 22:31:45 +1000

    - Modularized message printing
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

---
+++ scripts/tests/util/print_separator.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+#######################################
+# Prints a basic separator to stdout
+# Arguments:
+#  None
+#######################################
+function print_separator() {
+  echo "---------------------------------------"
+}
