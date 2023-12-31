85dfddd8b52754dc93ed1733f812e008c8c008d3
void <yane.clarke@gmail.com>
2023-12-31 16:54:06 +1000

    Summary:     - Formatting & refactored print message functionality ⚡
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>
    
    Took 3 seconds

--- scripts/operations/util/configure_server_files.sh
+++ scripts/operations/util/configure_server_files.sh
@@ -7,16 +7,16 @@ function configure_server_files() {
   print_messages "Creating server configuration files..."
   configure_backend_dotenv
   configure_frontend_dotenv
-  echo "Configuring the Docker Express..."
+  print_messages "Configuring the Docker Express..."
   generate_backend_dockerfile
-  echo "Configuring the site map..."
+  print_messages "Configuring the site map..."
   configure_frontend_sitemap
-  echo "Configuring default robots.txt..."
+  print_messages "Configuring default robots.txt..."
   configure_frontend_robots
-  echo "Configuring the frontend Docker environment..."
+  print_messages "Configuring the frontend Docker environment..."
   generate_frontend_dockerfile
-  echo "Configuring the Docker Certbot Image..."
+  print_messages "Configuring the Docker Certbot Image..."
   generate_certbot_dockerfile
-  echo "Configuring Docker Compose..."
+  print_messages "Configuring Docker Compose..."
   generate_docker_compose
 }
