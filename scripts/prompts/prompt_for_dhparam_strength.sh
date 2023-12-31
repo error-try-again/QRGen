6219c611eaa2698a4e94d6e176fec356c1e461f3
void <yane.clarke@gmail.com>
2023-12-31 16:33:00 +1000

    Summary:     - Refactored the print message functionality ⚡
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>
    
    Took 10 seconds

--- scripts/prompts/prompt_for_dhparam_strength.sh
+++ scripts/prompts/prompt_for_dhparam_strength.sh
@@ -13,10 +13,10 @@ set -euo pipefail
 #######################################
 function prompt_for_dhparam_strength() {
   if [[ -n "${DH_PARAM_SIZE:-}" ]]; then
-    echo "DH_PARAM_SIZE is already set to ${DH_PARAM_SIZE}. Skipping prompt."
+    print_messages "DH_PARAM_SIZE is already set to ${DH_PARAM_SIZE}. Skipping prompt."
     return
   fi
-  echo "1: Use 2048-bit DH parameters (Faster)"
+  print_messages "1: Use 2048-bit DH parameters (Faster)"
   echo "2: Use 4096-bit DH parameters (More secure)"
   prompt_numeric "Please enter your choice (1/2): " DH_PARAM_CHOICE
   case ${DH_PARAM_CHOICE} in
