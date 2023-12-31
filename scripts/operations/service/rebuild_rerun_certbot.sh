c09d3d670cdd20839cecf154346e8b1f9fad1244
void <yane.clarke@gmail.com>
2023-12-31 17:04:32 +1000

    Summary:     - Formatting & refactored print message functionality ⚡
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>
    
    Took 5 seconds

--- scripts/operations/service/rebuild_rerun_certbot.sh
+++ scripts/operations/service/rebuild_rerun_certbot.sh
@@ -10,9 +10,9 @@ set -euo pipefail
 #   1 ...
 #######################################
 function rebuild_and_rerun_certbot() {
-  echo "Rebuilding and rerunning Certbot without dry-run..."
+  print_messages "Rebuilding and rerunning Certbot without dry-run..."
   if ! docker compose build certbot || ! docker compose up -d certbot; then
-    echo "Failed to rebuild or run Certbot service."
+    print_messages "Failed to rebuild or run Certbot service."
     return 1
   fi
 }
