e1f67b52bdc26a9d3aca86543fcd28d67f213a9a
void <yane.clarke@gmail.com>
2023-12-31 17:00:22 +1000

    Summary:     - Formatting & refactored print message functionality ⚡
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>
    
    Took 2 seconds

--- scripts/operations/networking/ensure_port_is_available.sh
+++ scripts/operations/networking/ensure_port_is_available.sh
@@ -16,12 +16,12 @@ function is_port_in_use() {
   local port="$1"
   # Check if the port is a number.
   if ! [[ ${port} =~ ^[0-9]+$ ]]; then
-    echo "Error: Port must be a number."
+    print_messages "Error: Port must be a number."
     return 2 # Return a different exit code for invalid input.
   fi
 
   # Check if the port is in use. Using netcat (nc) as it is more commonly available.
-  if nc -z 127.0.0.1 "${port}" >/dev/null 2>&1; then
+  if nc -z 127.0.0.1 "${port}" > /dev/null 2>&1; then
     return 0 # Port is in use
   else
     return 1 # Port is not in use
@@ -44,12 +44,12 @@ function ensure_port_is_available() {
       ((port++))
     else
       local alternate_port
-      echo "Port ${port} is already in use."
+      print_messages "Port ${port} is already in use."
       read -rp "Please provide an alternate port or Ctrl+C to exit: " alternate_port
       port="${alternate_port:-${port}}"
     fi
   done
   # Set the EXPOSED_NGINX_PORT to the selected port that is not in use.
   EXPOSED_NGINX_PORT="${port}"
-  echo "Selected port ${EXPOSED_NGINX_PORT} is available."
+  print_messages "Selected port ${EXPOSED_NGINX_PORT} is available."
 }
