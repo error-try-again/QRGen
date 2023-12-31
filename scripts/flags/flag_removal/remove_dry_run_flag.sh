8b9568cc999ecd3a7e9dbf1d3f138301cb44d856
void <yane.clarke@gmail.com>
2023-12-31 16:33:14 +1000

    Summary:     - Refactored the print message functionality ⚡
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>
    
    Took 5 seconds

--- scripts/flags/flag_removal/remove_dry_run_flag.sh
+++ scripts/flags/flag_removal/remove_dry_run_flag.sh
@@ -12,7 +12,7 @@ set -euo pipefail
 #######################################
 function remove_dry_run_flag() {
   local temp_file
-  echo "Removing --dry-run flag from docker-compose.yml..."
+  print_messages "Removing --dry-run flag from docker-compose.yml..."
   temp_file=$(remove_certbot_command_flags_compose '--dry-run')
   check_flag_removal "${temp_file}" '--dry-run'
   backup_and_replace_file "${DOCKER_COMPOSE_FILE}" "${temp_file}"
