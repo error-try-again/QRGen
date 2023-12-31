5a47d425515d739238801dfdf77b47e6ea048548
void <yane.clarke@gmail.com>
2023-12-31 16:33:04 +1000

    Summary:     - Refactored the print message functionality ⚡
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>
    
    Took 4 seconds

--- scripts/prompts/prompt_for_domain_details.sh
+++ scripts/prompts/prompt_for_domain_details.sh
@@ -19,28 +19,29 @@ set -euo pipefail
 function prompt_for_domain_details() {
   if [[ ${SKIP_DOMAIN_PROMPT} == "true" ]]; then
     print_messages "Skipping domain prompt, please ensure that the following environment variables are set correctly:"
-    print_messages "BACKEND_SCHEME: ${BACKEND_SCHEME}" "USE_CUSTOM_DOMAIN: ${USE_CUSTOM_DOMAIN}"
-    print_messages "DOMAIN_NAME: ${DOMAIN_NAME}" "SUBDOMAIN: ${SUBDOMAIN}"
-    print_messages "ORIGIN: ${ORIGIN}" "ORIGIN_PORT: ${ORIGIN_PORT}"
+    print_messages "BACKEND_SCHEME: ${BACKEND_SCHEME}" \
+      "USE_CUSTOM_DOMAIN: ${USE_CUSTOM_DOMAIN}" \
+      "DOMAIN_NAME: ${DOMAIN_NAME}" "SUBDOMAIN: ${SUBDOMAIN}" \
+      "ORIGIN: ${ORIGIN}" "ORIGIN_PORT: ${ORIGIN_PORT}"
     return
-fi
+  fi
   prompt_yes_no "Would you like to specify a domain name other than the default (http://localhost)" USE_CUSTOM_DOMAIN
   if [[ ${USE_CUSTOM_DOMAIN} == "true" ]]; then
     DOMAIN_NAME=$(prompt_with_validation "Enter your domain name (e.g., example.com): " "Error: Domain name cannot be empty.")
     local origin_url="${BACKEND_SCHEME}://${DOMAIN_NAME}"
     ORIGIN="${origin_url}:${ORIGIN_PORT}"
-    echo "Using custom domain name: ${origin_url}"
+    print_messages "Using custom domain name: ${origin_url}"
 
     prompt_yes_no "Would you like to specify a subdomain other than the default (none)" USE_SUBDOMAIN
     if [[ ${USE_SUBDOMAIN} == "true" ]]; then
       SUBDOMAIN=$(prompt_with_validation "Enter your subdomain name (e.g., www): " "Error: Subdomain name cannot be empty.")
       origin_url="${BACKEND_SCHEME}://${SUBDOMAIN}.${DOMAIN_NAME}"
       ORIGIN="${origin_url}:${ORIGIN_PORT}"
-      echo "Using custom subdomain: ${origin_url}"
+      print_messages "Using custom subdomain: ${origin_url}"
     fi
   else
     DOMAIN_NAME="localhost"
     ORIGIN="${BACKEND_SCHEME}://${DOMAIN_NAME}:${ORIGIN_PORT}"
-    echo "Using default domain name: ${ORIGIN}"
+    print_messages "Using default domain name: ${ORIGIN}"
   fi
 }
