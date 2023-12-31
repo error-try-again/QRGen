e3d69a1f26c39d7d3b886bccd957dc8d1823cad8
void <yane.clarke@gmail.com>
2023-12-31 17:04:18 +1000

    Summary:     - Formatting & refactored print message functionality ⚡
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>
    
    Took 4 seconds

--- scripts/prompts/prompt_user_selection_switch.sh
+++ scripts/prompts/prompt_user_selection_switch.sh
@@ -9,14 +9,14 @@ set -euo pipefail
 #######################################
 function prompt_user_selection_switch() {
   case $1 in
-  "Run Setup") setup ;;
-  "Run Mock Configuration") mock ;;
-  "Uninstall") uninstall ;;
-  "Dump logs") dump_logs ;;
-  "Update Project") update_project ;;
-  "Stop Project Docker Containers") stop_containers ;;
-  "Purge Current Builds - Dangerous") purge ;;
-  "Quit") quit ;;
-  *) echo "Invalid selection: $1" ;;
-  esac
+    "Run Setup") setup ;;
+    "Run Mock Configuration") mock ;;
+    "Uninstall") uninstall ;;
+    "Dump logs") dump_logs ;;
+    "Update Project") update_project ;;
+    "Stop Project Docker Containers") stop_containers ;;
+    "Purge Current Builds - Dangerous") purge ;;
+    "Quit") quit ;;
+    *) print_messages "Invalid selection: $1" ;;
+esac
 }
