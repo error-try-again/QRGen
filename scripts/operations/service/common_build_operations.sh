a5ef6d2b09e92c466f9d993f9b80632946363636
void <yane.clarke@gmail.com>
2023-12-31 16:53:40 +1000

    Summary:     - Formatting & refactored print message functionality ⚡
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>
    
    Took 5 seconds

--- scripts/operations/service/common_build_operations.sh
+++ scripts/operations/service/common_build_operations.sh
@@ -12,32 +12,32 @@ set -euo pipefail
 #######################################
 function common_build_operations() {
   cd "${PROJECT_ROOT_DIR}" || {
-    echo "Failed to change directory to ${PROJECT_ROOT_DIR}"
+    print_messages "Failed to change directory to ${PROJECT_ROOT_DIR}"
     exit 1
   }
 
   pre_flight || {
-    echo "Failed pre-flight checks"
+    print_messages "Failed pre-flight checks"
     exit 1
   }
 
   # If Docker Compose is running, bring down the services
   # Ensure that old services are brought down before proceeding
-  if docker compose ps &>/dev/null; then
-    echo "Bringing down existing Docker Compose services..."
+  if docker compose ps &> /dev/null; then
+    print_messages "Bringing down existing Docker Compose services..."
     docker compose down || {
-      echo "Failed to bring down existing Docker Compose services"
+      print_messages "Failed to bring down existing Docker Compose services"
       exit 1
     }
   fi
 
   handle_certs || {
-    echo "Failed to handle certs"
+    print_messages "Failed to handle certs"
     exit 1
   }
 
   # Run each service separately - must be active for certbot to work
-  if [[ ${RELEASE_BRANCH} = "full-release" ]]; then
+  if [[ ${RELEASE_BRANCH} == "full-release" ]]; then
     run_backend_service
     run_frontend_service
   else
