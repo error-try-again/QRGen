db4513405156c9ed271098f0be5ab03fc1f954cd
void <yane.clarke@gmail.com>
2023-12-31 17:45:11 +1000

    Summary:     - Patched typo from replace all ⚡
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>
    
    Took 17 minutes

--- scripts/setup/docker/compose/assemble_docker_compose.sh
+++ scripts/setup/docker/compose/assemble_docker_compose.sh
@@ -10,43 +10,43 @@ set -euo pipefail
 #######################################
 function assemble_docker_compose_configuration() {
 
-  backend_service_definition=$(create_service_definition \
-    --name "${backend_name}" \
-    --build-context "${backend_context}" \
-    --dockerfile "${backend_dockerfile}" \
-    --ports "${backend_ports}" \
-    --volumes "${backend_volumes}" \
-    --networks "${backend_networks}" \
-    --restart "${backend_restart}" \
-    --depends-on "${backend_depends_on}")
+	backend_service_definition=$(create_service_definition \
+		--name "${backend_name}" \
+		--build-context "${backend_context}" \
+		--dockerfile "${backend_dockerfile}" \
+		--ports "${backend_ports}" \
+		--volumes "${backend_volumes}" \
+		--networks "${backend_networks}" \
+		--restart "${backend_restart}" \
+		--depends-on "${backend_depends_on}")
 
-  frontend_service_definition=$(create_service_definition \
-    --name "${frontend_name}" \
-    --build-context "${frontend_context}" \
-    --dockerfile "${frontend_dockerfile}" \
-    --ports "${frontend_ports}" \
-    --volumes "${frontend_volumes}" \
-    --networks "${frontend_networks}" \
-    --restart "${frontend_restart}" \
-    --depends-on "${frontend_depends_on}")
+	frontend_service_definition=$(create_service_definition \
+		--name "${frontend_name}" \
+		--build-context "${frontend_context}" \
+		--dockerfile "${frontend_dockerfile}" \
+		--ports "${frontend_ports}" \
+		--volumes "${frontend_volumes}" \
+		--networks "${frontend_networks}" \
+		--restart "${frontend_restart}" \
+		--depends-on "${frontend_depends_on}")
 
-  network_definition=$(create_network_definition \
-    "${network_name}" \
-    "${network_driver}")
+	network_definition=$(create_network_definition \
+		"${network_name}" \
+		"${network_driver}")
 
-  volume_definition=$(create_volume_definition \
-    "${volume_name}" \
-    "${volume_driver}")
+	volume_definition=$(create_volume_definition \
+		"${volume_name}" \
+		"${volume_driver}")
 
-  {
-    print_messages "version: '3.8'"
-    print_messages "services:"
-    echo "${backend_service_definition}"
-    echo "${frontend_service_definition}"
-    echo "${certbot_service_definition}"
-    echo "${network_definition}"
-    echo "${volume_definition}"
-  } > "${DOCKER_COMPOSE_FILE}"
+	{
+		echo "version: '3.8'"
+		echo "services:"
+		echo "${backend_service_definition}"
+		echo "${frontend_service_definition}"
+		echo "${certbot_service_definition}"
+		echo "${network_definition}"
+		echo "${volume_definition}"
+	} >"${DOCKER_COMPOSE_FILE}"
 
-   "Docker Compose configuration written to ${DOCKER_COMPOSE_FILE}"
+print_messages "Docker Compose configuration written to ${DOCKER_COMPOSE_FILE}"
 }
