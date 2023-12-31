ae47e7195202e234da26525b0b02afc9b0eec227
void <yane.clarke@gmail.com>
2023-12-31 17:47:05 +1000

    Summary:     - Patched typo in directory/file creation  ⚡
    Signed-off-by: void <yane.karov@gmail.com>
    
    Took 17 seconds
    
    Signed-off-by: void <yane.clarke@gmail.com>
    
    Took 44 seconds

--- scripts/tests/init/setup_common_mock_parameters.sh
+++ scripts/tests/init/setup_common_mock_parameters.sh
@@ -19,30 +19,30 @@ set -euo pipefail
 #  None
 #######################################
 function setup_common_mock_parameters() {
-  local default_root="${PROJECT_ROOT_DIR:-$(pwd)}"
-  test_output_dir="${default_root}/test_output"
+	local default_root="${PROJECT_ROOT_DIR:-$(pwd)}"
+	test_output_dir="${default_root}/test_output"
 
-  # Create directories if they don't exist
-  mkdir -p "${test_output_dir}}"
-  mkdir -p "${test_output_dir}}/logs"
-  mkdir -p "${test_output_dir}}/run"
+	# Create directories if they don't exist
+	mkdir -p "${test_output_dir}"
+	mkdir -p "${test_output_dir}/logs"
+	mkdir -p "${test_output_dir}/run"
 
-  # Ensure log and pid files exist
-  touch "${test_output_dir}}/logs/error.log"
-  touch "${test_output_dir}}/run/nginx.pid"
+	# Ensure log and pid files exist
+	touch "${test_output_dir}/logs/error.log"
+	touch "${test_output_dir}/run/nginx.pid"
 
-  BACKEND_PORT=12345
-  BACKEND_UPSTREAM_NAME="localhost"
-  NGINX_PID="pid ${PROJECT_ROOT_DIR}/nginx.pid;"
-  NGINX_ERROR_LOG="error_log ${test_output_dir}/logs/error.log warn;"
-  NGINX_ACCESS_LOG="access_log ${test_output_dir}/logs/access.log;"
-  RELEASE_BRANCH="full-release"
-  NODE_VERSION="latest"
-  NGINX_SSL_PORT="443"
-  EXPOSED_NGINX_PORT=8080
-  BACKEND_PORT=12345
-  DNS_RESOLVER="8.8.8.8"
-  TIMEOUT="5"
-  DOMAIN_NAME="example.com"
-  SUBDOMAIN="test"
+	BACKEND_PORT=12345
+	BACKEND_UPSTREAM_NAME="localhost"
+	NGINX_PID="pid ${PROJECT_ROOT_DIR}/nginx.pid;"
+	NGINX_ERROR_LOG="error_log ${test_output_dir}/logs/error.log warn;"
+	NGINX_ACCESS_LOG="access_log ${test_output_dir}/logs/access.log;"
+	RELEASE_BRANCH="full-release"
+	NODE_VERSION="latest"
+	NGINX_SSL_PORT="443"
+	EXPOSED_NGINX_PORT=8080
+	BACKEND_PORT=12345
+	DNS_RESOLVER="8.8.8.8"
+	TIMEOUT="5"
+	DOMAIN_NAME="example.com"
+	SUBDOMAIN="test"
 }
