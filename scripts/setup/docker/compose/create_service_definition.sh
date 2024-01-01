1cd01d3d2012cdebcb2c18c4db09c6a568f3b97f
void <yane.clarke@gmail.com>
2024-01-01 15:40:23 +1000

    Summary:     - Refactored service definition creation functions for maintainability ⚡
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

--- scripts/setup/docker/compose/create_service_definition.sh
+++ scripts/setup/docker/compose/create_service_definition.sh
@@ -3,121 +3,117 @@
 set -euo pipefail
 
 #######################################
-# Adds a ports, volumes or networks definition to the service definition.
-# Globals:
-#   ADDR
-#   item
+# Adds a ports, volumes, or networks definition to the service definition.
 # Arguments:
-#   1
-#   2
+#   type: The type of definition being added (ports, volumes, or networks)
+#   values: The comma-separated values to be added under the type
+# Outputs:
+#   Part of a service definition to be concatenated later.
 #######################################
 function add_to_definition() {
-	local type=$1
-	local values=$2
-	local definition
+  local type=$1
+  local values=$2
+  local definition_part=""
 
-	if [[ -n ${values} ]]; then
-		definition+=$'\n'
-		definition+="    ${type}:"
-		IFS=',' read -ra ADDR <<<"${values}"
-		local item
-		for item in "${ADDR[@]}"; do
-			if [[ -n ${item} ]]; then
-				definition+=$'\n'
-				definition+="      - ${item}"
-			fi
-		done
-	fi
+  if [[ -n ${values} ]]; then
+    definition_part+=$'\n'"    ${type}:"
+    local items
+    IFS=',' read -ra items <<<"${values}"
+    local item
+    for item in "${items[@]}"; do
+      if [[ -n ${item} ]]; then
+        definition_part+=$'\n'"      - ${item}"
+      fi
+    done
+  fi
+  echo "${definition_part}"
 }
 
 #######################################
-# Created a generic service definition for Docker Compose file.
-# Globals:
-#   ADDR
+# Creates a generic service definition for Docker Compose file.
 # Arguments:
-#  None
+#   Named arguments for various parts of the service definition
+# Outputs:
+#   The service definition.
 #######################################
 function create_service_definition() {
-	local name=""
-	local build_context=""
-	local dockerfile=""
-	local command=""
-	local ports=""
-	local volumes=""
-	local networks=""
-	local restart=""
-	local depends=""
+  local name=""
+  local build_context=""
+  local dockerfile=""
+  local command=""
+  local ports=""
+  local volumes=""
+  local networks=""
+  local restart=""
+  local depends=""
 
-	# Parsing named arguments
-	while [[ $# -gt 0 ]]; do
-		case $1 in
-		--name)
-			name=$2
-			shift 2
-			;;
-		--build-context)
-			build_context=$2
-			shift 2
-			;;
-		--dockerfile)
-			dockerfile=$2
-			shift 2
-			;;
-		--command)
-			command=$2
-			shift 2
-			;;
-		--ports)
-			ports=$2
-			shift 2
-			;;
-		--volumes)
-			volumes=$2
-			shift 2
-			;;
-		--networks)
-			networks=$2
-			shift 2
-			;;
-		--restart)
-			restart=$2
-			shift 2
-			;;
-		--depends-on)
-			depends=$2
-			shift 2
-			;;
-		*) shift ;;
-		esac
-	done
+  # Parsing named arguments
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+    --name)
+      name=$2
+      shift 2
+      ;;
+    --build-context)
+      build_context=$2
+      shift 2
+      ;;
+    --dockerfile)
+      dockerfile=$2
+      shift 2
+      ;;
+    --command)
+      command=$2
+      shift 2
+      ;;
+    --ports)
+      ports=$2
+      shift 2
+      ;;
+    --volumes)
+      volumes=$2
+      shift 2
+      ;;
+    --networks)
+      networks=$2
+      shift 2
+      ;;
+    --restart)
+      restart=$2
+      shift 2
+      ;;
+    --depends-on)
+      depends=$2
+      shift 2
+      ;;
+    *) # Unknown option
+      echo "Warning: Ignored unknown option: $1"
+      shift
+      ;;
+    esac
+  done
 
-	# Building the service definition
-	local definition
-	definition="  ${name}:"
-	definition+=$'\n'
-	definition+="    build:"
-	definition+=$'\n'
-	definition+="      context: ${build_context}"
-	definition+=$'\n'
-	definition+="      dockerfile: ${dockerfile}"
+  # Building the service definition
+  local definition="  ${name}:"
+  definition+=$'\n'"    build:"
+  definition+=$'\n'"      context: ${build_context}"
+  definition+=$'\n'"      dockerfile: ${dockerfile}"
 
-	if [[ -n ${command} ]]; then
-		definition+=$'\n'
-		definition+="    command: ${command}"
-	fi
+  if [[ -n ${command} ]]; then
+    definition+=$'\n'"    command: ${command}"
+  fi
 
-	add_to_definition "ports" "${ports}"
-	add_to_definition "volumes" "${volumes}"
-	add_to_definition "networks" "${networks}"
+  definition+="$(add_to_definition "ports" "${ports}")"
+  definition+="$(add_to_definition "volumes" "${volumes}")"
+  definition+="$(add_to_definition "networks" "${networks}")"
 
-	if [[ -n ${restart} ]]; then
-		definition+=$'\n'
-		definition+="    restart: ${restart}"
-	fi
+  if [[ -n ${restart} ]]; then
+    definition+=$'\n'"    restart: ${restart}"
+  fi
 
-	if [[ -n ${depends} ]]; then
-		add_to_definition "depends_on" "${depends}"
-	fi
+  if [[ -n ${depends} ]]; then
+    definition+="$(add_to_definition "depends_on" "${depends}")"
+  fi
 
 echo "${definition}"
 }
