978bbe13d4725d34c514e45a4dc86f9e95e8cc51
void <yane.clarke@gmail.com>
2024-01-01 15:39:14 +1000

    Summary:     - Formatting ⚡
    Signed-off-by: void <yane.karov@gmail.com>
    Signed-off-by: void <yane.clarke@gmail.com>

--- scripts/setup/docker/compose/generate_docker_compose.sh
+++ scripts/setup/docker/compose/generate_docker_compose.sh
@@ -10,12 +10,13 @@
 #  None
 #######################################
 function generate_docker_compose() {
-print_messages "Configuring Docker Compose..."
+  print_messages "Configuring Docker Compose..."
   initialize_variables
   backup_existing_config "${DOCKER_COMPOSE_FILE}"
   if [[ ${USE_LETSENCRYPT} == "true" ]]; then
     configure_compose_letsencrypt_mode
   elif [[ ${USE_SELF_SIGNED_CERTS} == "true" ]]; then
+echo "Configuring Docker Compose for self-signed certificates...!!!"
     configure_compose_self_signed_mode
   else
     configure_http
