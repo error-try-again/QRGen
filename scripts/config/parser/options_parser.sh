8087e0ac03c707c8397bc5ca627ec0cea436ab8d
void <yane.clarke@gmail.com>
2024-01-02 17:23:34 +1000

    Summary:     - Reduced scope of flag assignment feature
    Signed-off-by: void <yane.karov@gmail.com>
    
    Took 1 minute
    
    Signed-off-by: void <yane.clarke@gmail.com>
    
    Took 18 seconds

--- scripts/config/parser/options_parser.sh
+++ scripts/config/parser/options_parser.sh
@@ -3,42 +3,14 @@
 
 set -euo pipefail
 
-#######################################
-# This function assigns a value to a global flag variable.
-# It strips leading double-dashes and replaces hyphens with underscores in the flag name.
-# If no value is provided for the flag, it defaults to true.
-# Arguments:
-#   1: flag name (string) - the name of the flag variable to assign.
-#   2: optional value for flag (string) - the value to assign to the flag variable; defaults to 'true'.
-# Exits:
-#   1 - If the flag name does not match expected pattern.
-#######################################
-function assign_flag_value() {
-  # Transform flag name to adhere to variable naming conventions:
-  # Strip leading double-dash and replace hyphens with underscores.
-  local flag_name="${1//--/}"   # Strip leading double-dash
-  flag_name="${flag_name//-/_}" # Replace hyphens with underscores
-  local flag_value="${2:-true}" # Default flag value to 'true' if no value provided
-
-  # Validate that flag_name adheres to expected naming conventions:
-  # Starts with 'use_' or 'no_', followed by lowercase letters or underscores.
-  if ! [[ ${flag_name} =~ ^(use_|no_)?[a-z_]+$ ]]; then
-    print_messages "Error: Unexpected flag '${flag_name}'"
-    exit 1
-  fi
-
-  # Declare a global variable dynamically using the name and value provided.
-  declare -g "${flag_name}"="${flag_value}"
-}
-
-#######################################
-# This function parses the script options using getopt.
-# It supports both short and long options, defining behavior for known flags.
-# Uses 'getopt' to parse the provided options and sets up an options processing loop.
+########################################
+# Parse the script options using getopt.
+# Support both short and long options, defining behavior for known flags.
+# Use 'getopt' to parse the provided options and set up an options processing loop.
 # Arguments:
 #   None
-# Exits:
-#   1 - If options parsing fails.
+# Returns:
+#   None
 #######################################
 function options_parser() {
 
@@ -46,17 +18,7 @@ function options_parser() {
 
   # Define long options for the script. Each option is followed by a comma.
   # Options requiring an argument are followed by a colon.
-  long_options_list="setup,mock,uninstall,dump-logs,update-project,\
-stop-containers,purge,quit,use-hsts,use-ocsp-stapling,use-tls13,use-tls12,help\
-backend-port:,nginx-port:,nginx-ssl-port:,challenge-port:,\
-node-version:,dns-resolver:,timeout:,domain-name:,backend-scheme:,\
-subdomain:,regenerate-ssl-certs:,letsencrypt-email:,use-production-ssl:,\
-use-lets-encrypt:,use-custom-domain:,use-must-staple:,use-strict-permissions:,\
-use-uir:,use-dry-run:,use-auto-renew-ssl:,use-overwrite-self-signed-certs:,\
-use-force-renew:,use-self-signed-certs:,use-ssl:,dh-param-size:,tos-flag:,\
-no-eff-email-flag:,non-interactive-flag:,rsa-key-size:,build-certbot-image:,\
-disable-docker-caching:,use-google-api-key:,google-maps-api-key:,release-branch:,use-gzip:,\
-"
+  long_options_list="setup,mock,uninstall,dump-logs,update-project,help,quit,stop-containers,purge"
 
   # Parse the provided options using getopt. This prepares them for consumption.
   # -o defines short options, here only 'h' (help) as a short option.
@@ -72,30 +34,56 @@ disable-docker-caching:,use-google-api-key:,google-maps-api-key:,release-branch:
     exit 1
   fi
 
-  # Evaluate the parsed options string to set the positional parameters ($1, $2, etc.)
   eval set -- "${parsed_options}"
   while true; do
     case "$1" in
       --help | -h)
-        print_messages "Usage instructions for the script..."
-        exit 0
+        # Display help message and terminate script execution.
+        display_help
+        ;;
+      --setup)
+        # Perform setup and terminate script execution.
+        setup
+        ;;
+      --mock)
+        # Perform mock function and terminate script execution.
+        mock
+        ;;
+      --uninstall)
+        # Perform uninstall function and terminate script execution.
+        uninstall
+        ;;
+      --stop-containers)
+        # Stop containers and terminate script execution.
+        stop_containers
+        ;;
+      --purge)
+        # Perform purge function and terminate script execution.
+        purge
+        ;;
+      --dump-logs)
+        # Dump logs and terminate script execution.
+        dump_logs
+        ;;
+      --update-project)
+        # Update project and terminate script execution.
+        update_project
+        ;;
+      --quit)
+        # Terminate script execution.
+        quit
         ;;
       --)
-        shift
+        # End of options.
         break
         ;;
       *)
-        # Default case: handle long option or its argument.
-        # If the second parameter looks like another option or if it's empty, consider it a boolean flag.
-        # Otherwise, treat it as an argument for an option.
-        if [[ $2 =~ ^-.* || -z $2 ]]; then
-          assign_flag_value "$1"
-    else
-          assign_flag_value "$1" "$2"
-          shift # Move past the argument as it's been handled.
-    fi
+        # Unexpected option.
+        print_messages "Unexpected option: $1" >&2
+        quit
         ;;
-esac
-    shift # Move to the next parameter or option.
-  done
+  esac
+    # Proceed to next option if available.
+    shift
+done
 }
