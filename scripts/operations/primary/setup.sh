e71620ab5edaaa2754034129413b1bac7c9f7aeb
void <yane.clarke@gmail.com>
2024-01-02 17:22:18 +1000

    Summary:     - Refactored setup stage to be more modular
    Signed-off-by: void <yane.karov@gmail.com>
    
    Took 1 minute
    
    Signed-off-by: void <yane.clarke@gmail.com>
    
    Took 30 seconds

--- scripts/operations/primary/setup.sh
+++ scripts/operations/primary/setup.sh
@@ -3,6 +3,33 @@
 set -euo pipefail
 
 #######################################
+# description
+# Arguments:
+#   1
+#######################################
+function handle_skip_prompts() {
+  local bypass
+  bypass=$1
+  if [[ ${bypass} == true ]]; then
+      print_messages "Skipping prompts..."
+  fi
+}
+
+# Handle auto install
+function handle_auto_install() {
+  prompt_for_auto_install
+  if [[ ${AUTO_INSTALL} == true ]]; then
+      select_and_apply_profile "${INSTALL_PROFILE}"
+  else
+    prompt_for_release_install_mode
+    prompt_disable_docker_build_cache
+    prompt_for_domain_and_letsencrypt
+    prompt_for_google_api_key
+    prompt_for_gzip
+  fi
+}
+
+#######################################
 # Setup project directories and configurations.
 # Globals:
 # EXPOSED_NGINX_PORT
@@ -10,20 +37,12 @@ set -euo pipefail
 #  None
 #######################################
 function setup() {
-  setup_project_directories
-  setup_docker_rootless
-  ensure_port_is_available "${EXPOSED_NGINX_PORT}" "auto"
-  if [[ ${PROMPT_BYPASS} == true ]]; then
-    print_messages "Skipping prompts..."
-  else
-    select_and_apply_profile "${INSTALL_PROFILE}"
-    prompt_for_release_install_mode
-    prompt_disable_docker_build_cache
-    prompt_for_domain_and_letsencrypt
-    prompt_for_google_api_key
-    prompt_for_gzip
-  fi
-  configure_server_files
-  generate_nginx_config
-  build_and_run_docker
+    setup_project_directories
+    setup_docker_rootless
+    ensure_port_is_available "${EXPOSED_NGINX_PORT}" "auto"
+    handle_skip_prompts "${PROMPT_BYPASS}"
+    handle_auto_install
+    configure_server_files
+    generate_nginx_config
+    build_and_run_docker
 }
