85844af437c916972c5d51f93a4e135996d702ab
void <yane.clarke@gmail.com>
2024-01-03 11:01:55 +1000

    Cleanup:     - Reformatting     - Adjusted flag configuration to ensure logical consistency
    Signed-off-by: void <yane.karov@gmail.com>
    
    Took 1 minute
    
    Signed-off-by: void <yane.clarke@gmail.com>
    
    Took 24 seconds

--- scripts/operations/certbot/build_certbot_service.sh
+++ scripts/operations/certbot/build_certbot_service.sh
@@ -10,7 +10,7 @@
 #   1 ...
 #######################################
 function build_certbot_service() {
-  if [[ -n ${DISABLE_DOCKER_CACHING} ]]; then
+  if [[ -n "${DISABLE_DOCKER_CACHING}" ]] && [[ "${DISABLE_DOCKER_CACHING}" == "true" ]]; then
     print_messages "Building Certbot service without caching..."
     if ! docker compose --progress=plain build --no-cache certbot; then
       print_messages "Failed to build Certbot service."
