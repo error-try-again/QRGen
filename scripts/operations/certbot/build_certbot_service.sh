23c86966091ffc48f4ee4a34508891959979f208
Yane Karov <yane.karov@gmail.com>
2024-01-03 18:18:56 +1000

    Cleanup:     - Patched the certbot build logic to be more consistent
    Signed-off-by: Yane Karov <yane.karov@gmail.com>
    
    Took 3 minutes

--- scripts/operations/certbot/build_certbot_service.sh
+++ scripts/operations/certbot/build_certbot_service.sh
@@ -1,20 +1,33 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 #######################################
-# Builds the custom Certbot service image.
+# Builds and runs the Certbot service using docker compose.
+# Arguments:
+#   1
+#######################################
+function docker_compose_certbot_service() {
+  local cache_option
+  cache_option="${1:-}"
+  if [[ -n "${cache_option}" ]]; then
+    docker compose --progress=plain build "${cache_option}" certbot;
+  else
+    docker compose --progress=plain build certbot;
+  fi
+}
+
+#######################################
+# Determines whether Docker's caching system should be used when building the Certbot service image.
 # Globals:
 #   DISABLE_DOCKER_CACHING
 # Arguments:
 #  None
-# Returns:
-#   1 ...
 #######################################
-function build_certbot_service() {
-  if [[ -n "${DISABLE_DOCKER_CACHING}" ]] && [[ "${DISABLE_DOCKER_CACHING}" == "true" ]]; then
-    print_messages "Building Certbot service without caching..."
-    if ! docker compose --progress=plain build --no-cache certbot; then
-      print_messages "Failed to build Certbot service."
-      return 1
-    fi
+function run_certbot_service() {
+  if [[ -n ${DISABLE_DOCKER_CACHING} ]] && [[ ${DISABLE_DOCKER_CACHING} == 'true' ]]; then
+    docker_compose_certbot_service '--no-cache'
+  else
+    docker_compose_certbot_service
   fi
 }
