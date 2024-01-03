5c36fa53356e8dfe8de1803baa3b91f43a8199c3
void <yane.clarke@gmail.com>
2024-01-03 11:01:31 +1000

    Cleanup:     - Reformatting
    Signed-off-by: void <yane.karov@gmail.com>
    
    Took 1 minute
    
    Signed-off-by: void <yane.clarke@gmail.com>
    
    Took 1 hour 5 minutes

--- scripts/tests/util/append_timestamps_to_log.sh
+++ scripts/tests/util/append_timestamps_to_log.sh
@@ -11,7 +11,7 @@ function append_timestamps_to_log() {
     print_messages "Error: ${log_file} does not exist!"
     return 1
   fi
-awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' "${log_file}" >"${log_file}.tmp" &&
-  mv "${log_file}.tmp" "${log_file}" ||
-  echo "Error: Failed to process file!"
+  awk '{ print strftime("[%Y-%m-%d %H:%M:%S]"), $0 }' "${log_file}" > "${log_file}.tmp" \
+                                                                                     && mv "${log_file}.tmp" "${log_file}" \
+                                     || echo "Error: Failed to process file!"
 }
