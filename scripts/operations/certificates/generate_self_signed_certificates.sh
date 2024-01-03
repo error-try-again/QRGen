1f267165c334a694dcce7f9f9ca4498c64939911
void <yane.clarke@gmail.com>
2024-01-03 11:04:19 +1000

    Cleanup:     - Isolated & resolved logical consistency issues     - Modularized functions
    Signed-off-by: void <yane.karov@gmail.com>
    
    Took 1 minute
    
    Signed-off-by: void <yane.clarke@gmail.com>
    
    Took 53 seconds

--- scripts/operations/certificates/generate_self_signed_certificates.sh
+++ scripts/operations/certificates/generate_self_signed_certificates.sh
@@ -11,15 +11,17 @@ set -eo pipefail
 # Arguments:
 #  None
 #######################################
-function generate_self_signed_certificates() {
+function handle_self_signed_certificates() {
   print_messages "Generating self-signed certificates for ${DOMAIN_NAME}..."
   local certs_path="${CERTS_DIR}/live/${DOMAIN_NAME}"
   local fullchain_path="${certs_path}/fullchain.pem"
   local privkey_path="${certs_path}/privkey.pem"
+
   create_directory "${certs_path}"
   create_directory "${CERTS_DH_DIR}"
+
   check_and_generate_certificates "${fullchain_path}" "${privkey_path}"
-  generate_dh_parameters
+  handle_dh_param_generation "${CERTS_DH_DIR}/dhparam.pem"
 }
 
 #######################################
@@ -34,12 +36,10 @@ function generate_self_signed_certificates() {
 function check_and_generate_certificates() {
   local fullchain_path="$1"
   local privkey_path="$2"
-  if [[ ! -f "${fullchain_path}" ]] || [[ ${REGENERATE_SSL_CERTS} == "true" ]]; then
-    openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
-      -keyout "${privkey_path}" \
-      -out "${fullchain_path}" \
-      -subj "/CN=${DOMAIN_NAME}"
-    print_messages "Private key generated at ${privkey_path}." "Certificate generated at ${fullchain_path}."
+
+  # If the certificates don't exist, or if the user wants to regenerate them, generate them.
+  if [[ ! -f "${fullchain_path}" ]] || [[ ! -f "${privkey_path}" ]] || [[ ${REGENERATE_SSL_CERTS} == "true" ]]; then
+    generate_self_signed_certificates "${privkey_path}" "${fullchain_path}"
   else
     print_messages "Certificates for ${DOMAIN_NAME} already exist at ${fullchain_path} and ${privkey_path}." "Skipping certificate generation."
   fi
@@ -53,9 +53,36 @@ function check_and_generate_certificates() {
 #   DH_PARAMS_FILE
 #######################################
 function generate_dh_parameters() {
+  local DH_PARAMS_FILE="$1"
+
   print_messages "Generating a Diffie-Hellman (DH) key exchange parameters file with ${DH_PARAM_SIZE} bits..."
   openssl dhparam -out "${DH_PARAMS_FILE}" "${DH_PARAM_SIZE}"
   print_messages "DH parameters generated at ${DH_PARAMS_FILE}."
+}
+
+#######################################
+# Generates self-signed certificates for the given domain.
+# Globals:
+#   DOMAIN_NAME
+# Arguments:
+#   1
+#   2
+#######################################
+function generate_self_signed_certificates() {
+  local privkey_path="$1"
+  local fullchain_path="$2"
+
+  print_messages "Generating self-signed certificates for ${DOMAIN_NAME}..."
+  openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
+    -keyout "${privkey_path}" \
+    -out "${fullchain_path}" \
+    -subj "/CN=${DOMAIN_NAME}"
+  if [[ ! -f "${fullchain_path}" ]] || [[ ! -f "${privkey_path}" ]]; then
+    print_messages "Something went wrong while generating the certificates. Please try again."
+    exit 1
+  else
+    print_messages "Certificates generated at ${fullchain_path} and ${privkey_path}."
+  fi
 }
 
 #######################################
@@ -68,21 +95,27 @@ function generate_dh_parameters() {
 #######################################
 function handle_dh_param_generation() {
   local DH_PARAMS_FILE="$1"
+
   if [[ ! -f "${DH_PARAMS_FILE}" ]]; then
-    print_messages "DH parameters file not found at ${DH_PARAMS_FILE}. Generating new DH parameters."
+    print_messages "DH parameters file not found at ${DH_PARAMS_FILE}. Generating new DH parameters..."
     prompt_for_dhparam_strength
-    generate_dh_parameters
-  elif [[ "${REGENERATE_SSL_CERTS}" == "true" ]]; then
-    generate_dh_parameters
-  elif [[ "${REGENERATE_SSL_CERTS}" == "false"  ]]; then
-    prompt_for_dhparam_regeneration && generate_dh_parameters
+    generate_dh_parameters "${DH_PARAMS_FILE}"
+  fi
+
+  if [[ "${REGENERATE_SSL_CERTS}" == "true" ]]; then
+    generate_dh_parameters "${DH_PARAMS_FILE}"
+    generate_self_signed_certificates
+  fi
+
+  if [[ "${REGENERATE_SSL_CERTS}" == "false"  ]]; then
+    prompt_for_dh_param_regeneration
   else
     print_messages "Hmm.. Couldn't determine if DH parameters need to be generated. Skipping."
   fi
 }
 
 #######################################
-# Prompts the user to select whether they want to regenerate the SSL certificates/DH Parameters.
+# Prompts the user to select whether they want to regenerate the DH Parameters.
 # Globals:
 #   REGENERATE_SSL_CERTS
 # Arguments:
@@ -91,8 +124,26 @@ function handle_dh_param_generation() {
 #   0 if need to regenerate
 #   1 otherwise
 #######################################
-function prompt_for_dhparam_regeneration() {
-  read -rp "Do you want to regenerate the certificates? [y/N]: " response
+function prompt_for_dh_param_regeneration() {
+  read -rp "Do you want to regenerate the dh parameters? [y/N]: " response
+  if [[ "${response}" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+      prompt_for_dhparam_strength
+      generate_dh_parameters "${DH_PARAMS_FILE}"
+  else
+    print_messages "Skipping DH parameters generation."
+  fi
+}
+
+#######################################
+# Prompts the user to select whether they want to regenerate the self-signed certificates.
+# Arguments:
+#  None
+# Returns:
+#   0 ...
+#   1 ...
+#######################################
+function prompt_for_certificate_regeneration() {
+  read -rp "Do you want to regenerate the self-signed certificates? [y/N]: " response
   if [[ "${response}" =~ ^([yY][eE][sS]|[yY])$ ]]; then
     return 0
   else
@@ -109,7 +160,7 @@ function prompt_for_dhparam_regeneration() {
 #  None
 #######################################
 function prompt_for_dhparam_strength() {
-  if [[ -n ${DH_PARAM_SIZE:-} ]]; then
+  if [[ ${DH_PARAM_SIZE} -eq 2048 || ${DH_PARAM_SIZE} -eq 4096 ]]; then
     print_messages "DH Parameters size is already set to ${DH_PARAM_SIZE}. Skipping prompt."
     return
   fi
