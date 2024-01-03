ba018c62d5fc69f9f04361fdccdb826d26e49c97
Yane Karov <yane.karov@gmail.com>
2024-01-03 18:14:10 +1000

    Cleanup:     - Patched the build frontend logic to be more consistent
    Signed-off-by: Yane Karov <yane.karov@gmail.com>
    
    Took 54 seconds

--- scripts/operations/service/run_frontend_service.sh
+++ scripts/operations/service/run_frontend_service.sh
@@ -3,29 +3,32 @@
 set -euo pipefail
 
 #######################################
-# Builds and runs the frontend service
-# Checks to see if caching is disabled, and builds accordingly
+# Builds and runs the frontend service using docker compose.
+# Arguments:
+#   1
+#######################################
+function docker_compose_frontend_service() {
+  local cache_option
+  cache_option="${1:-}"
+  if [[ -n "${cache_option}" ]]; then
+    docker compose --progress=plain build "${cache_option}" frontend;
+  else
+    docker compose --progress=plain build frontend;
+  fi
+  docker compose up -d frontend
+}
+
+#######################################
+# Determines whether Docker's caching system should be used when building the Frontend service image.
 # Globals:
 #   PROJECT_ROOT_DIR
 # Arguments:
 #  None
 #######################################
 function run_frontend_service() {
-  if [[ -n ${DISABLE_DOCKER_CACHING} ]] && [[ ${DISABLE_DOCKER_CACHING} == "true" ]]; then
-    print_messages "Building frontend service with caching disabled..."
-    if ! docker compose --progress=plain build --no-cache frontend; then
-      print_messages "Failed to build Frontend service."
-      exit 1
-    fi
-    print_messages "Running Frontend service..."
-    docker compose up -d frontend 2>&1 | grep -Po 'port \K\d+' | xargs -I {} lsof -i :{} -S
+  if [[ -n ${DISABLE_DOCKER_CACHING} ]] && [[ ${DISABLE_DOCKER_CACHING} == 'true' ]]; then
+    docker_compose_frontend_service '--no-cache'
   else
-    print_messages "Building frontend service..."
-    if ! docker compose --progress=plain build frontend; then
-      print_messages "Failed to build Frontend service."
-      exit 1
-    fi
-    print_messages "Running Frontend service..."
-    docker compose up -d frontend 2>&1 | grep -Po 'port \K\d+' | xargs -I {} lsof -i :{} -S
+    docker_compose_frontend_service
   fi
 }
