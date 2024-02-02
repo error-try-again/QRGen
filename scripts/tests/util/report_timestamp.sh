f853efd9b9a000f6b97ce3bbe6a04e8a1f37cd6a
Yane Karov <yane.karov@gmail.com>
2024-02-03 09:04:17 +1000

    Summary:
      - Cleaned up timestamping

--- scripts/tests/util/report_timestamp.sh
+++ scripts/tests/util/report_timestamp.sh
@@ -9,16 +9,9 @@ set -euo pipefail
 #  $1 - The date format (default "[%Y-%m-%d %H:%M:%S]")
 #  $2 - The timezone (default the system's timezone)
 #######################################
-function report_timestamp() {
-  local time_format=${1:-"%Y-%m-%d_%H:%M:%S"}
-  local tz=${2:-""}
-
-  if [[ -z ${tz} ]]; then
-    local time
-    time=$(date +"${time_format}")
-  else
-    local time
-    time=$(TZ=${tz} date +"${time_format}")
-  fi
+report_timestamp() {
+  local time_format="%Y-%m-%d_%H:%M:%S"
+  local time
+  time=$(date +"${time_format}")
   echo "${time}"
 }
